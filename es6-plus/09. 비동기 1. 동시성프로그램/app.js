93f94f2c048f450d57f65aa6234c6d8a2c014369
goodjwon <jwon76@naver.com>
2020-04-06 22:37:57 +0900

    Promise 정의

---
+++ es6-plus/09. 비동기 1. 동시성프로그램/app.js
@@ -0,0 +1,45 @@
+const promise = new Promise((resolve, reject) =>{
+    if(true) {
+        resolve('처리결과')
+    } else {
+        reject('오류메세지;')
+    }
+})
+
+
+
+const promise2 = new Promise((resolve, reject)=>{
+    let succeeded = false;
+    console.log('처리 중');
+    succeeded = true;
+
+    if(succeeded) {
+        resolve('처리결과')
+    }else{
+        reject('오류메세지')
+    }
+})
+
+promise2.then(result=>{
+    console.log(result)
+}).catch(error=>{
+    console.log(error)
+});
+
+
+const promise3 = new Promise((resolve, reject)=>{
+    console.log('promise3');
+    resolve('promise3 처리 완료')
+})
+
+const promise4 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log('promise4');
+
+        resolve('promise4 처리 완료');
+    }, 1000)
+})
+
+Promise.all([promise3, promise4]).then(results =>{
+    console.log(results);
+})
